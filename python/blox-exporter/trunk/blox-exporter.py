23bd0e6aa0ef0521485e0e572e76529427b3ba4f
kosciak1 <kosciak1@3aabf250-ba9b-11de-bb27-3175f2afd961>
2010-04-08 14:25:31 +0000

    forced UTF-8 encoding for generated files

--- python/blox-exporter/trunk/blox-exporter.py
+++ python/blox-exporter/trunk/blox-exporter.py
@@ -48,7 +48,7 @@ HTML_JAVASCRIPT = '''
 HTML_START = '''<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=%(ENCODING)s"/> 
+    <meta http-equiv="Content-Type" content="text/html; charset="UTF-8"/> 
     <title>%(TITLE)s</title>
     %(HTML_STYLE)s
     %(HTML_JAVASCRIPT)s
