9cf2bc774bbc64263b954c4ca5a046f5f225396b
kosciak1 <kosciak1@3aabf250-ba9b-11de-bb27-3175f2afd961>
2010-05-26 17:07:29 +0000

    new version after blipi.pl redesign

--- greasemonkey/blippl_-_rdirpl_stats_and_reblipi.user.js
+++ greasemonkey/blippl_-_rdirpl_stats_and_reblipi.user.js
@@ -3,80 +3,85 @@
 // @namespace      http://userscripts.org/users/20935
 // @description    Rdir.pl stats and re.blipi.pl mashup
 // @author         Wojciech 'KosciaK' Pietrzok
-// @version        0.3.1
+// @version        0.4
 // @include        http://rdir.pl/*/stats
 // ==/UserScript==
 
-function $$(xpath,root) { 
-  xpath = xpath
-    .replace(/((^|\|)\s*)([^/|\s]+)/g,'$2.//$3')
-    .replace(/\.([\w-]+)(?!([^\]]*]))/g, '[@class="$1" or @class$=" $1" or @class^="$1 " or @class~=" $1 "]')
-    .replace(/#([\w-]+)/g, '[@id="$1"]')
-    .replace(/\/\[/g,'/*[');
-  str = '(@\\w+|"[^"]*"|\'[^\']*\')';
-  xpath = xpath
-    .replace(new RegExp(str+'\\s*~=\\s*'+str,'g'), 'contains($1,$2)')
-    .replace(new RegExp(str+'\\s*\\^=\\s*'+str,'g'), 'starts-with($1,$2)')
-    .replace(new RegExp(str+'\\s*\\$=\\s*'+str,'g'), 'substring($1,string-length($1)-string-length($2)+1)=$2');
-  var got = document.evaluate(xpath, root||document, null, 5, null), result=[];
-  while (next = got.iterateNext())
-    result.push(next);
-  return result;
- }
-
-
-
 (function() {
-    url = document.documentURI
-    rdir_id = url.substring(15, url.lastIndexOf("/"))
-    reblipi_url = "http://re.blipi.pl/" + rdir_id
+    url = document.documentURI;
+    rdir_id = url.substring(15, url.lastIndexOf("/"));
+    reblipi_url = "http://re.blipi.pl/" + rdir_id;
 
-    GM_addStyle("#loading { display: block; margin: 1em auto; }")
-    GM_addStyle(".avatar {width: 15px; height: 15px;} br {margin-bottom: 1em;} .show { font-size: smaller; }")
+    GM_addStyle("#loading { display: block; margin: 1em auto; }");
+    GM_addStyle(".avatar {margin: 0 0.25em 0 0.5em; vertical-align:middle;} br {margin-bottom: 1em;} ");
     
-    box = $$(".module")[0]
+    box = document.evaluate('//*[@class="module"]', 
+                            document, null, 
+                            XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, 
+                            null).snapshotItem(0); 
 
-    header = document.createElement('h3')
-    header.innerHTML = 'Magia RE.BLIPI <a href="' + reblipi_url + '">' + reblipi_url + '</a>'
+    header = document.createElement('h3');
+    header.innerHTML = 'Magia reblipi <a href="' + reblipi_url + '">' + 
+                       reblipi_url + '</a>';
 
-    loading = document.createElement('img')
-    loading.setAttribute('id', 'loading')
-    loading.setAttribute('src', 'http://blip.pl/images/ajax-loading.gif')
+    loading = document.createElement('img');
+    loading.setAttribute('id', 'loading');
+    loading.setAttribute('src', 'http://blip.pl/images/ajax-loading.gif');
 
-    box.appendChild(header)
-    box.appendChild(loading)
+    box.appendChild(header);
+    box.appendChild(loading);
 
     GM_xmlhttpRequest({
         method: "GET",
         url: "http://re.blipi.pl/" + rdir_id,
         onload: function(response) {
-            var responseXML = document.createElement('html')
-            responseXML.innerHTML = response.responseText
-
+            var responseXML = document.createElement('html');
+            responseXML.innerHTML = response.responseText;
+            
             results = document.createElement('ul');
-
-            active = $$("tr", responseXML)
-            last_td = active[active.length-1];
-            if (last_td.childNodes[1].className != "head") {
-                results.innerHTML = '<li class="normal margin"><li>' + last_td.innerHTML + "</li>"
+            addedBy = document.createElement('li');
+            addedBy.innerHTML = 'Dodane przez: <br />';
+            results.appendChild(addedBy);
+            
+            quotes = document.evaluate('//li[@class="content"]/p', 
+                                 responseXML, null, 
+                                 XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, 
+                                 null); 
+            
+            for (i=0; i < quotes.snapshotLength ; i++) {
+                quote = quotes.snapshotItem(i);
+                item = document.createElement('li');
+                
+                link = quote.parentNode.parentNode
+                            .getElementsByTagName('a')[1];
+                
+                img = quote.parentNode.parentNode
+                           .getElementsByTagName('img')[0];
+                
+                name = img.getAttribute('title');
+                name = name.substring(1, name.length);
+                
+                image = document.createElement('a');
+                image.setAttribute('href', 'http://blip.pl/users/'+ name +'/dashboard');
+                image.appendChild(img);
+                
+                author = document.createElement('a');
+                author.setAttribute('href', 'http://blip.pl/users/'+ name +'/dashboard');
+                author.innerHTML = name;
+                
+                content = document.createElement('span');
+                content.innerHTML = ': ' + quote.innerHTML
+                
+                item.appendChild(link);
+                item.appendChild(image);
+                item.appendChild(author);
+                item.appendChild(content);
+                item.appendChild(document.createElement('br'));
+                
+                results.appendChild(item);
             }
-            box.replaceChild(results, loading)
-            /*
-            for (i=active.length-1; i>=0; i--) {
-                last_td = active[i];
-                alert(last_td.attributes.getNamedItem("colspan").value)
-                if (last_td.attributes.getNamedItem("colspan")) {
-                    results.innerHTML = '<li class="normal margin">Napisali:</li><li>' + last_td.innerHTML + "</li>"
-
-                    box.replaceChild(results, loading)
-                    break;
-                }
-            }
-            */
+            box.replaceChild(results, loading);
         }
     });
 
 })();
-
-
-
